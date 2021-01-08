a2dd5d76836209074270584c88bb111a058ba563
Michael Pucket <michael_puckett_ii@hotmail.com>
2021-01-08 18:45:58 -0500

    updated property mapping to same types.

---
+++ ImmutaMap/ImmutaMapperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImmutaMap
+{
+    public static class ImmutaMapperExtensions
+    {
+        public static T With<T>(this T t, Func<dynamic> func)
+        {
+            var a = func.Invoke();
+            var properties = new List<(string Name, object Value)>();
+            foreach (var prop in a.GetType().GetProperties())
+            {
+                var foundProp = typeof(T).GetProperty(prop.Name);
+                if (foundProp != null) properties.Add((prop.Name, prop.GetValue(a, null)));
+            }
+
+            return ImmutaMapper.Build(mapper =>
+            {
+                foreach (var (Name, Value) in properties) mapper.WithSourceProperty(Name, () => Value);
+            })
+            .Map<T>(t);
+        }
+    }
+}
