758731cae1e686b92fbc03329dcaf67246ec456c
Michael Pucket <michael_puckett_ii@hotmail.com>
2021-02-11 20:12:26 -0500

    swapping branch

---
+++ ImmutaMap/Interfaces/ITypeFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImmutaMap.Interfaces
+{
+    /// <summary>
+    /// Represents a type that can get the instance of a value T.
+    /// </summary>
+    public interface ITypeFormatter
+    {
+        T GetInstance<T>(Func<object[]> args = null);
+    }
+}
--- ImmutaMap/Mapper.cs
+++ ImmutaMap/Mapper.cs
@@ -1,31 +1,80 @@
-﻿using System;
+﻿using ImmutaMap.Interfaces;
+using Specky.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace ImmutaMap
 {
+    [Speck]
     public class Mapper
     {
+        private readonly ITypeFormatter typeFormatter;
+
+        public Mapper(ITypeFormatter typeFormatter)
+        {
+            this.typeFormatter = typeFormatter;
+        }
+
         public Map<TSource, TResult> Map<TSource, TResult>(TSource source) => new Map<TSource, TResult>(source);
-        public static TResult Build<TSource, TResult>(Map<TSource, TResult> map)
+
+        public TResult Build<TSource, TResult>(Map<TSource, TResult> map, Func<object[]> args = null)
         {
             TResult result;
+            result = typeFormatter.GetInstance<TResult>(args);
 
-            try
+            Copy(map, result);
+
+            return result;
+        }
+
+        private void Copy<TSource, TResult>(Map<TSource, TResult> map, TResult result)
+        {
+            var sourcePropertyInfos = typeof(TSource).GetProperties().ToList();
+            var resultPropertyInfos = typeof(TResult).GetProperties().ToList();
+
+            foreach (var sourcePropertyInfo in sourcePropertyInfos)
             {
-                result = Activator.CreateInstance<TResult>();
+                foreach (var resultPropertyInfo in resultPropertyInfos)
+                {           
+                    foreach (var propertyMap in map.PropertyMaps)
+                    {
+                        if (propertyMap.SourceProperty == sourcePropertyInfo.Name
+                        && propertyMap.ResultProperty == resultPropertyInfo.Name)
+                        {
+
+                        }
+                    }
+                    if (sourcePropertyInfo.Name == resultPropertyInfo.Name)
+                    {
+                        resultPropertyInfo.SetValue(result, sourcePropertyInfo.GetValue(map.Source));
+                        break;
+                    }
+
+
+                }
             }
-            catch
-            {
-                result = Create
-            }
+        }
     }
 
     public class Map<TSource, TResult>
     {
+        private readonly List<(string SourceProperty, string ResultProperty)> propertyMaps = new List<(string SourceProperty, string ResultProperty)>();
         public Map(TSource source)
         {
             Source = source;
         }
 
+        public IEnumerable<(string SourceProperty, string ResultProperty)> PropertyMaps => propertyMaps.ToList();
+
         public TSource Source { get; }
+
+        public void MapProperty(Expression<Func<TSource, object>> sourceExpression, Expression<Func<TResult, object>> resultExpression)
+        {
+            if (sourceExpression.Body is MemberExpression sourceMemberExpression
+            && resultExpression.Body is MemberExpression resultMemberExpression)
+            propertyMaps.Add((sourceMemberExpression.Member.Name, resultMemberExpression.Member.Name));
+        }
     }
 }
