2f5bb9a0866724333ba826100b0e625f7499c819
Michael Brian Puckett, II <michael.puckett.ii@outlook.com>
2023-04-03 19:08:01 -0400

    moved file and renamed mapconfiguration to replaced map

--- ImmutaMap/Map.cs
+++ ImmutaMap/Map.cs
@@ -130,25 +130,3 @@ public class Map<TSource, TTarget> where TSource : notnull where TTarget : notnu
     /// </summary>
     public static Map<TSource, TTarget> Empty { get; } = new Map<TSource, TTarget>();
 }
-
-public class Mapper<TSource, TTarget> where TSource : notnull where TTarget : notnull
-{
-    internal Mapper(TSource source, Map<TSource, TTarget> configuration)
-    {
-        Configuration = configuration;
-        Source = source;
-    }
-
-    public Map<TSource, TTarget> Configuration { get; }
-    public TSource Source { get; }
-
-    /// <summary>
-    /// Builds the Map, invoking all mappings added.  Takes the source values and places them in the targets values.
-    /// </summary>
-    /// <param name="map">The Map this method works against.</param>
-    /// <returns>The result of the mapping as an instantiated TTarget Type.</returns>
-    public TTarget Build()
-    {
-        return MapBuilder.GetNewInstance().Build(Configuration, Source);
-    }
-}
